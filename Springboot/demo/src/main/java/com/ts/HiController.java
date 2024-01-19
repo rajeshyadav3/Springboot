20d3899484559fb24939a75ac5bea2970d8e1b5d
Yarragorlla Rajesh <yrajeshyrajeshyadav@gmail.com>
2024-01-19 10:05:15 +0530

    done with crud operations

---
+++ Springboot/demo/src/main/java/com/ts/HiController.java
@@ -0,0 +1,19 @@
+package com.ts;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HiController {
+	
+	@RequestMapping("sayHi")
+	public String sayHi(){
+		return "Hi Everyone!!!";
+	}
+	
+	@RequestMapping("sayHello")
+	public String sayHello(){
+		return "Hello Everyone!!!";
+	}
+
+}
